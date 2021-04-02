8590dcdda3b2cf8f914b41be02270cc66868207c
Maxie Wright <maxiewright@gmail.com>
2021-04-02 16:22:47 -0400

    modify 503 page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,7 +2,7 @@
     <div class="bg-white rounded-lg shadow-lg p-5 md:p-20 mx-2">
         <div class="text-center">
             <h2 class="text-4xl tracking-tight leading-10 font-bold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-                Coming Soon
+                We'll Be Back Soon
             </h2>
             <p class="text-md md:text-xl mt-10">
                 <a class="hover:underline" href="https://www.mentoringheroes.com">
@@ -11,6 +11,12 @@
                 make your journey throughout life better.</p>
         </div>
         <div class="flex flex-wrap mt-10 justify-center">
+            <div class="m-3">
+                <a href="https://www.pintrest.com/mentoringheroes" title="Mentoring Heroes On Pintrest"
+                   class="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-blue-600 hover:border-blue-600 hover:bg-blue-600 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+                    <span class="mx-auto">Pintrest</span>
+                </a>
+            </div>
             <div class="m-3">
                 <a href="https://www.facebook.com/mentoringheroes" title="Mentoring Heroes On Facebook"
                    class="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-blue-600 hover:border-blue-600 hover:bg-blue-600 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
