4a5f0aedd230dd17e046b678deb6de52fe6059d1
Maxie Wright <maxiewright@gmail.com>
2021-04-02 16:19:58 -0400

    modify main layout template

--- resources/views/components/input/text.blade.php
+++ resources/views/components/input/text.blade.php
@@ -14,3 +14,20 @@
     >
     @error($model)<div class="text-red-500"> {{$message}} </div> @enderror
 </div>
+
+@props([
+    'type',
+    'name',
+    'placeholder',
+    'model' => null
+])
+
+<div {{$attributes->merge(['class' => ''])}}>
+
+    <input wire:model="{{$model}}"
+           type="{{$type}}"
+           placeholder="{{$placeholder}}"
+           class="border p-2 w-full @error($model) border-red-500 @enderror "
+    >
+    @error($model)<div class="text-red-500"> {{$message}} </div> @enderror
+</div>
