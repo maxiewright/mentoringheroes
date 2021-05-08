d4dbea20394f88e4e88a7444b511ccb954313920
Maxie Wright <maxiewright@gmail.com>
2021-05-08 08:33:02 -0400

    add .idea folder to git ignore file

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -42,10 +42,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return in_array($user->email, [
-                'maxiewright@gmail.com',
-                'khsankara@gmail.com'
-            ]);
+            return $user->hasAnyRole('admin', 'editor', 'author','contributor');
         });
     }
 
