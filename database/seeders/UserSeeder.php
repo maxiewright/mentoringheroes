16e667a0d185bdc60641d5c20de660eaafc863a5
Maxie Wright <maxiewright@gmail.com>
2021-05-24 09:39:04 -0400

    modify User Seeder to seed roles

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Hash;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -15,6 +16,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('users')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
         $users = [
             [
                 'name' => 'Maxie Wright',
@@ -31,5 +36,11 @@ class UserSeeder extends Seeder
         ];
 
         User::insert($users);
+
+        $admins = User::all();
+
+        foreach ($admins as $admin){
+            $admin->assignRole('admin');
+        }
     }
 }
