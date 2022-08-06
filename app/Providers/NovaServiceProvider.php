807a84ea4fa7a9f0d30fb3a287e56db6c2bb88cc
Maxie Wright <maxiewright@gmail.com>
2022-08-06 10:46:52 -0400

    update nova service provide to incule auth emails

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -43,7 +43,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                //
+                'maxiewright@gmail.com',
+                'khsankara@gmail.com'
             ]);
         });
     }
